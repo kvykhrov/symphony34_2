0e0bebf2780f9c10a9f653fd45f992fc873f083b
“kvykhrov” <kvykhrov@gmail.com>
2020-02-24 20:02:53 +0200

    changes gitignore folder screenshots

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+
+
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
